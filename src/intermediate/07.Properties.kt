268ff4349caa3ff0a0211c2d3985d8b33b906011
chaeros <chpark6851@koreatech.ac.kr>
2025-06-13 23:59:42 +0900

    feat : [Intermediate] 7. Properties - Standard delegates 1) Lazy properties 2) Observable properties

--- src/intermediate/07.Properties.kt
+++ src/intermediate/07.Properties.kt
@@ -1,6 +1,6 @@
 package intermediate
 
-import kotlin.reflect.KProperty
+import kotlin.properties.Delegates.observable
 
 fun properties() {
     val person = Person()
@@ -15,6 +15,19 @@ fun properties() {
     val user = User("park","chaeros")
     println(user.displayName)   // Computed and cached: park chaeros
     println(user.displayName)   // Accessed from cache: park chaeros
+
+    // Standard delegates - Lazy properties
+    fetchData()
+    // Connected to database
+    // Data : [Data1, Data2, Data3]
+    fetchData()
+    // Data : [Data1, Data2, Data3]
+
+    // Standard delegates - Observable properties
+    val thermostat = Thermostat()
+    thermostat.temperature = 23.1   // Temperature updated : 20.0°C->23.1°C
+
+    thermostat.temperature = 29.5   // Warning : Temperature is too high! (23.1°C->29.5°C)
 
     // Exercise 1
     val inventory = listOf(3,0,7,0,5)
@@ -81,6 +94,49 @@ class CachedStringDelegate {
         }
         return cachedValue ?: "Unknown"
     }
+}
+
+// Standard delegates - Lazy properties
+class Database {
+    fun connect() {
+        println("Connected to database")
+    }
+    fun query(sql:String): List<String> {
+        return listOf("Data1","Data2","Data3")
+    }
+}
+
+val databaseConnection: Database by lazy {
+    // lazy로 선언된 속성으로 처음 이 속성에 접근할 때만 Database()객체가 생성되고 이후로는 단순 호출
+    // 이로인해 해상 속성이 사용되지 않는 경우 객체를 생성하지않음
+    // 초기화 비용이 크거나 무거운 경우 효율적으로 사용가능
+    // lazy는 기본적으로 Synchronized를 제공해 thread-safe 하여 멀티 스레드 환경에서도 안전하게 초기화 가능
+    // db 에 Database 객체를 생성하고, 메서드 실행 후 db 객체 반환
+    val db = Database()
+    db.connect()
+    db
+}
+
+// Standard delegates - Observable properties
+// 속성 값이 바뀔 때 이를 감지해서 특정 동작을 수행하고 싶을 때 사용
+// import kotlin.properties.Delegates.observable 하여야 사용 가능
+// 값이 변경될 때마다 실행할 코드를 람다식으로 작성
+// old는 이전 값, new는 새로운 값, _ 는 속성 이름 안씀, 변경값만 출력
+// 디버깅, UI 업데이트, 데이터 유효성 검사 등에 활용됨
+class Thermostat {
+    var temperature: Double by observable(20.0) { _, old, new ->
+        if ( new > 25 ) {
+            println("Warning : Temperature is too high! ($old°C->$new°C)")
+        }
+        else {
+            println("Temperature updated : $old°C->$new°C")
+        }
+    }
+}
+
+fun fetchData() {
+    val data = databaseConnection.query("SELECT * FROM user")
+    println("Data : ${data}")
 }
 
 // Exercise 1
